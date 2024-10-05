6ce3c308d5d62eeb06662443147a3bf3cb0d759f
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2024-10-05 15:18:01 +1000

    Update app.py

--- lambda/create_user/app.py
+++ lambda/create_user/app.py
@@ -5,6 +5,7 @@ from botocore.exceptions import ClientError
 dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table('Users')  # Replace with your DynamoDB table name
 
+
 def handler(event, context):
     try:
         data = json.loads(event['body'])
