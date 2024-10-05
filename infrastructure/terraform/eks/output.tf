4cc9d2cfde6d7fe5be8d3f469af3c0b805a76e44
xiongye77 <36766101+xiongye77@users.noreply.github.com>
2024-10-05 12:19:32 +1000

    Update output.tf

--- infrastructure/terraform/eks/output.tf
+++ infrastructure/terraform/eks/output.tf
@@ -1,15 +1,3 @@
-output "eks_cluster_endpoint" {
-  description = "Endpoint for the EKS cluster"
-  value       = module.eks.cluster_endpoint
-}
-
-output "eks_cluster_certificate_authority_data" {
-  description = "Certificate Authority data for the EKS cluster"
-  value       = module.eks.cluster_certificate_authority_data
-}
-
-ye.xiong@LM-M0D9GF902V eks-cluster % vi output.tf 
-ye.xiong@LM-M0D9GF902V eks-cluster % more eks/output.tf 
 output "cluster_endpoint" {
   description = "Endpoint for the EKS cluster"
   value       = module.eks.cluster_endpoint
